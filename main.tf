898ccf16fc7cba13c5afd9134cd9057b5bc5a2fe
Aareet Mahadevan <aareet@users.noreply.github.com>
2023-07-04 21:46:51 -0700

    Add cloud block for TFC

--- main.tf
+++ main.tf
@@ -1,10 +1,18 @@
 terraform {
+  cloud {
+    organization = "aareet"
+    workspaces {
+      name = "cloudflare"
+    }
+  }
+
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
       version = "4.9.0"
     }
   }
+  required_version = "~> 1.5"
 }
 
 provider "cloudflare" {
