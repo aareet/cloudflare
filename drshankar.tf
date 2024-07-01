40194da5e2df2bb4d330d9f311f7b55d5ad49ffc
Aareet Mahadevan <aareet@users.noreply.github.com>
2024-07-01 09:39:42 -0700

    Add dns records to root and www to enable page redirect

--- drshankar.tf
+++ drshankar.tf
@@ -1,5 +1,24 @@
-# NOTE: MX has been moved away from cloudflare
+resource "cloudflare_record" "terraform_managed_resource_drshankar_root_redirect" {
+  comment = "Dummy A record so that page rule will work."
+  name    = "drshankar.ca"
+  proxied = true
+  ttl     = 1
+  type    = "A"
+  value   = "192.0.2.1"
+  zone_id = data.cloudflare_zone.drshankar.id
+}
 
+resource "cloudflare_record" "terraform_managed_resource_drshankar_www_redirect" {
+  comment = "Dummy A record so that page rule will work."
+  name    = "www"
+  proxied = true
+  ttl     = 1
+  type    = "A"
+  value   = "192.0.2.1"
+  zone_id = data.cloudflare_zone.drshankar.id
+}
+
+# NOTE: MX has been moved away from cloudflare
 # DMARC analysis
 resource "cloudflare_record" "terraform_managed_resource_0413bd47491f52d0ff6333c1cd1e7fd6" {
   name    = "_dmarc"
